5a4e71ad7699685079f8da754fc44b115f948c9c
MK4self <mrinal3rajput@gmail.com>
2020-04-11 20:41:05 +1000

    Added key Cosmos DB details

--- ShardCode/config.js
+++ ShardCode/config.js
@@ -1,9 +1,9 @@
 const config = {
-    endpoint: "<Your Azure Cosmos account URI>",
-    key: "<Your Azure Cosmos account key>",
-    databaseId: "Tasks",
-    containerId: "Items",
-    partitionKey: { kind: "Hash", paths: ["/category"] }
+    endpoint: "https://cosmos-acc-openbankingapi.documents.azure.com:443/",
+    key: "RVwew5IiEGy3ktTIx8ikgtuWtGibnjLuZNxVg9BFnRPo8vR8O3QJTIRexSxMVuwfdisqAgERh3WhBPdXfsuWkQ==",
+    databaseId: "cosmos-db-openbankingapi",
+    containerId: "products",
+    partitionKey: { kind: "Hash", paths: ["/Type"] }
   };
   
   module.exports = config;
